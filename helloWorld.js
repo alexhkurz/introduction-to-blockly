ae7260725fe50dd26f3b0b20118be055bedaebb2
alexhkurz <alexhkurz@gmail.com>
2023-05-19 21:53:03 -0700

    send hello world removed

--- helloWorld.js
+++ helloWorld.js
@@ -3,14 +3,6 @@ const app = express();
 
 app.use(express.static('public'));
 // Allows the user to path to localhost:3000/<file in /public>
-// Example: localhost:3000/index1.html
-
-// Can send the html file, but does not know how to parse it
-// For "rendering" actual files, need to use a view engine like EJS or Pug
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-  // res.render('index') // Needs view engine
-});
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
